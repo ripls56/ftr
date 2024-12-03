13db5dba570f47e3b77abfede1b340383532de8a
ripls56 <84716344+ripls56@users.noreply.github.com>
2024-12-03 16:29:22 +0300

    Reapply "chore!(splitter): remove splitterv1"
    This reverts commit 187f541de6283381fd757995c656d6db28575efd.

--- splitter_bench_test.go
+++ splitter_bench_test.go
@@ -43,7 +43,8 @@ func BenchmarkSplit(b *testing.B) {
 			batchSize: 5,
 		},
 		{
-			name:     "Small file",
+			name: "Small file",
+			// 1 kbit
 			splitter: &FileSplitter{},
 			filePath: "testdata/small.txt",
 			// 1 kbit
@@ -117,124 +118,7 @@ func BenchmarkSplit(b *testing.B) {
 
 				origInfo, err := file.Stat()
 				assert.NoError(b, err)
-				ch := bm.splitter.Split(file, SplitOpts{
-					BatchSize: bm.batchSize,
-					FileName:  fileName,
-				})
-
-				for batch := range ch {
-					_, err := outFile.Write(batch.Content)
-					assert.NoError(b, err)
-				}
-
-				outInfo, err := outFile.Stat()
-				assert.NoError(b, err)
-
-				assert.Equal(b, origInfo.Size(), outInfo.Size())
-			}
-		})
-	}
-}
-
-func BenchmarkSplitV2(b *testing.B) {
-	benchmarks := []struct {
-		name      string
-		splitter  SplitV2
-		filePath  string
-		batchSize int
-	}{
-		{
-			name:      "Small file",
-			splitter:  &FileSplitterV2{},
-			filePath:  "testdata/small.txt",
-			batchSize: 1,
-		},
-		{
-			name:      "Small file",
-			splitter:  &FileSplitterV2{},
-			filePath:  "testdata/small.txt",
-			batchSize: 5,
-		},
-		{
-			name: "Small file",
-			// 1 kbit
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/small.txt",
-			// 1 kbit
-			batchSize: 1 << 10,
-		},
-		{
-			name:     "Small file",
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/small.txt",
-			// 0,5 kb
-			batchSize: 1 << 12,
-		},
-		{
-			name:      "Large jpg image",
-			splitter:  &FileSplitterV2{},
-			filePath:  "testdata/large-img.jpg",
-			batchSize: 128,
-		},
-		{
-			name:     "Large jpg image",
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/large-img.jpg",
-			// 0,5 kb
-			batchSize: 1 << 12,
-		},
-		{
-			name:     "Large jpg image",
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/large-img.jpg",
-			// 32 kb
-			batchSize: 1 << 18,
-		},
-		{
-			name:     "Large jpg image",
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/large-img.jpg",
-			// 128 kb
-			batchSize: 1 << 20,
-		},
-		{
-			name:     "Large jpg image",
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/large-img.jpg",
-			// 1 mbit
-			batchSize: 1 << 23,
-		},
-		{
-			name:     "Large jpg image",
-			splitter: &FileSplitterV2{},
-			filePath: "testdata/large-img.jpg",
-			// 2 mb
-			batchSize: 1 << 24,
-		},
-	}
-
-	for _, bm := range benchmarks {
-		name := fmt.Sprintf("%s batch size %d", bm.name, bm.batchSize)
-
-		b.Run(name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				file, err := os.Open(bm.filePath)
-				assert.NoError(b, err)
-				defer file.Close()
-
-				fileName := filepath.Base(bm.filePath)
-				outFile, err := os.CreateTemp(os.TempDir(), fileName)
-				assert.NoError(b, err)
-
-				defer outFile.Close()
-				defer os.Remove(outFile.Name())
-
-				origInfo, err := file.Stat()
-				assert.NoError(b, err)
-				err = bm.splitter.Split(file, outFile, SplitOpts{
-					BatchSize: bm.batchSize,
-					FileName:  fileName,
-				})
+				err = bm.splitter.Split(file, outFile, bm.batchSize)
 				assert.NoError(b, err)
 
 				outInfo, err := outFile.Stat()
